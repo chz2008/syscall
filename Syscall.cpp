4aafb3eb4ff0904df3016bcf01c97989458f7130
Zer08Byte <41458106+chz2008@users.noreply.github.com>
2024-10-16 12:17:33 +0800

    Update and rename SyscallNtTerminateProcess.cpp to Syscall.cpp

--- Syscall.cpp
+++ Syscall.cpp
@@ -1,8 +1,7 @@
 ﻿#include <iostream>
 #include <windows.h>
-#include <lm.h>
 
-#pragma comment(lib, "ntdll.lib")
+#pragma comment(lib, "ntdll")
 
 EXTERN_C NTSTATUS SysNtTerminateProcessFunc(
     HANDLE ProcessHandle,
@@ -19,20 +18,11 @@ void printUsage() {
 }
 
 int main(int argc, char* argv[]) {
-   //FARPROC addr = GetProcAddress(LoadLibraryA("ntdll"), "NtTerminateProcess");
-
-    //const wchar_t* username = L"NewUser";  // 要添加的用户名
-    //const wchar_t* password = L"Password123";  // 用户密码
-
-    //DWORD targetProcessId = 3860;
-
-    // 如果参数不足或用户请求帮助
     if (argc != 2 || strcmp(argv[1], "--help") == 0) {
         printUsage();
         return 1;
     }
 
-    // 将传入的参数转换为目标进程ID
     DWORD targetProcessId = atoi(argv[1]);
 
     if (targetProcessId == 0) {
@@ -40,7 +30,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // 打开目标进程的句柄，具有终止权限才行，杀不了比如360  火绒==，需要另外的方式，这里是为了学习直接syscall技术，kill av这里不做讨论
+    // 具有终止权限才行，杀不了比如360  火绒==，需要另外的方式，这里是为了学习syscall，kill av这里不做讨论
     //通过这个思路，这个可以做"其他好玩"的事情
     HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, targetProcessId);
     if (hProcess == NULL) {
@@ -48,10 +38,9 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // 调用汇编函数终止目标进程
-    NTSTATUS status = SysNtTerminateProcessFunc(hProcess, 0); // 0 表示正常退出
+    NTSTATUS status = SysNtTerminateProcessFunc(hProcess, 0); 
 
-    if (status == 0) { // STATUS_SUCCESS
+    if (status == 0) { 
         std::cout << "Process terminated successfully." << std::endl;
     }
     else {
@@ -60,10 +49,5 @@ int main(int argc, char* argv[]) {
 
     // 关闭进程句柄
     CloseHandle(hProcess);
-
-   // SysNtTerminateProcessFunc();
-
-    system("pause");
-
     return 0;
 }
